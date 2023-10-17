a573f546d1846adc98b3005ecf9165c6c2d6e6d1
tkprather <146144703+tkprather@users.noreply.github.com>
2023-10-17 22:53:38 +0000

    dd

--- app.rb
+++ app.rb
@@ -52,3 +52,71 @@ get("/dice/5/4") do
 
   erb(:five_four)
 end
+
+get("/") do
+  erb(:homepage)
+end
+
+get("/dice/2/6") do
+  @rolls = []
+
+  2.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
+
+  erb(:two_six)
+end
+
+get("/dice/2/10") do
+  @rolls = []
+
+  2.times do
+    die = rand(1..10)
+
+    @rolls.push(die)
+  end
+
+  erb(:two_ten)
+end
+
+get("/dice/1/20") do
+  @rolls = []
+
+  1.times do
+    die = rand(1..20)
+
+    @rolls.push(die)
+  end
+
+  erb(:one_twenty)
+end
+
+get("/dice/5/4") do
+  @rolls = []
+
+  5.times do
+    die = rand(1..4)
+
+    @rolls.push(die)
+  end
+
+  erb(:five_four)
+end
+
+get("/dice/:zebra/:giraffe") do
+  @num_dice = params.fetch("zebra").to_i
+
+  @sides = params.fetch("giraffe").to_i
+  
+  @rolls = []
+
+  @num_dice.times do
+    die = rand(1..@sides)
+
+    @rolls.push(die)
+  end
+
+  erb(:flexible)
+end
